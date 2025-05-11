4755a359caaf1848110acc11faac96505866c474
Naga Sri Sai Chaitanya Kolluri <saichaitanya518@gmail.com>
2025-05-12 00:55:34 +0530

    first commit

---
+++ src/test/java/com/srt/streamhive_auth/StreamhiveAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srt.streamhive_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamhiveAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
