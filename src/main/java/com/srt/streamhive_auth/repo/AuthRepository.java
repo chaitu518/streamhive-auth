85793d977eebe91ff0ad822d8bd3c3c87f51f2be
Naga Sri Sai Chaitanya Kolluri <saichaitanya518@gmail.com>
2025-05-12 13:05:46 +0530

    B Revert "login and token functionality implemented"
    This reverts commit 7c58fb9b2c003a85eaf6804da5ab1438c9e336cf.

--- src/main/java/com/srt/streamhive_auth/repo/AuthRepository.java
+++ src/main/java/com/srt/streamhive_auth/repo/AuthRepository.java
@@ -3,8 +3,6 @@ package com.srt.streamhive_auth.repo;
 import com.srt.streamhive_auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface AuthRepository extends JpaRepository<User, Long> {
-    public Optional<User> findByEmail(String email);
+    public User findByEmail(String email);
 }
