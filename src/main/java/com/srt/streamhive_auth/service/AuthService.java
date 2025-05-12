7b925cca435317d35b295835001b7cc972257511
Naga Sri Sai Chaitanya Kolluri <saichaitanya518@gmail.com>
2025-05-12 13:16:14 +0530

    login and token is added

--- src/main/java/com/srt/streamhive_auth/service/AuthService.java
+++ src/main/java/com/srt/streamhive_auth/service/AuthService.java
@@ -2,36 +2,48 @@ package com.srt.streamhive_auth.service;
 
 import com.srt.streamhive_auth.model.User;
 import com.srt.streamhive_auth.repo.AuthRepository;
+import com.srt.streamhive_auth.security.JwtUtil;
+import com.srt.streamhive_auth.security.SecurityConfig;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthService {
     private AuthRepository authRepository;
-    public AuthService(AuthRepository authRepository) {
+    private JwtUtil jwtUtil;
+    private PasswordEncoder passwordEncoder;
+    public AuthService(AuthRepository authRepository, JwtUtil jwtUtil) {
+
         this.authRepository = authRepository;
+        this.jwtUtil = jwtUtil;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
     public String registerUser(String email, String password) {
         User newUser = new User();
         newUser.setEmail(email);
-        newUser.setPassword(password);
+        newUser.setPassword(passwordEncoder.encode(password));
         User user = authRepository.save(newUser);
         if (user != null) {
-            return "User registered successfully";
+            String token = jwtUtil.generateToken(user.getEmail());
+            return token;
         }
         else {
-            return "User could not be registered";
+            return "Not registered";
         }
 
     }
 
     public String loginUser(String email, String password) {
-        User loginUser = authRepository.findByEmail(email);
-        if (loginUser == null) {
-            return "User could not be found";
+        Optional<User> userOpt = authRepository.findByEmail(email);
+        if (userOpt.isPresent() &&
+                new BCryptPasswordEncoder().matches(password, userOpt.get().getPassword())) {
+
+            String token = jwtUtil.generateToken(email);
+            return token;
         }
-        if (loginUser.getPassword().equals(password)) {
-            return "User logged in successfully";
-        }
-        return "User could not be logged in";
+        return "Invalid Credentials";
     }
 }
